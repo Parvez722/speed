e620b800bfe0be8ee576fd2fb48249c18626c0e9
Parvez722 <65571098+Parvez722@users.noreply.github.com>
2022-06-14 17:56:47 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
+pkg update
+pkg install python
 pip install speedtest-cli
 pip install git+https://github.com/sivel/speedtest-cli.git
 wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
 chmod +x speedtest-cli
-cp speed/* /
+cp /data/user/0/com.termux/files/home/speed/install.sh /data/user/0/com.termux/files/home/
+cd
+echo 'speedtest
+
+./run.sh' > run.sh
+chmod +x *
