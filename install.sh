b87ea9b5eb8b50e9d1f70de3e6cbf1d20b63b3d7
Parvez722 <65571098+Parvez722@users.noreply.github.com>
2022-06-14 17:34:37 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
-speedtest
-./install.sh
+pip install speedtest-cli
+pip install git+https://github.com/sivel/speedtest-cli.git
+wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+chmod +x speedtest-cli
+cp speed/* 
